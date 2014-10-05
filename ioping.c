a9eee379bff9b9337e373c300b6b66f7b26ece8a
Konstantin Khlebnikov <koct9i@gmail.com>
2014-10-05 12:57:54 +0400

    ioping: update changelog and dates
    Signed-off-by: Konstantin Khlebnikov <koct9i@gmail.com>

--- ioping.c
+++ ioping.c
@@ -1,7 +1,7 @@
 /*
  *  ioping  -- simple I/0 latency measuring tool
  *
- *  Copyright (C) 2011-2013 Konstantin Khlebnikov <koct9i@gmail.com>
+ *  Copyright (C) 2011-2014 Konstantin Khlebnikov <koct9i@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
